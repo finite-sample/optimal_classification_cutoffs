d7f296841303930197628c5554fddd16e7eb4a34
gaurav <721466+soodoku@users.noreply.github.com>
2025-08-17 13:29:48 -0700

    Add project metadata and package exports

---
+++ optimal_cutoffs/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for optimal classification cutoff utilities."""
+
+from optimal_cut_offs import get_confusion_matrix, get_probability
+
+__all__ = ["get_confusion_matrix", "get_probability"]
